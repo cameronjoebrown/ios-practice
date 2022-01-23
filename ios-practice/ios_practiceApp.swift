e4670fca3b0119178f7dc40a80e9ba6229e3d79e
Cameron Brown <cameronjoebrown@gmail.com>
2022-01-22 17:43:12 -0700

    Initial Commit

---
+++ ios-practice/ios_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_practiceApp.swift
+//  ios-practice
+//
+//  Created by Cam on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
